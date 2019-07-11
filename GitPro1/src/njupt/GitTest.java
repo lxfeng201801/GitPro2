8450b4fee647da7f8495324bd813014db30bbb86
admin123 <lxfeng02@163.com>
2019-07-11 17:01:22 +0800

    version1.2

--- GitPro1/src/njupt/GitTest.java
+++ GitPro1/src/njupt/GitTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class GitTest {
 	public static void main(String[] args) throws IOException {
-		 System.out.println("linegit1:");
+		 System.out.println("linegit1:"); 
+		 System.out.println("linegit2:");
 	}
 }
