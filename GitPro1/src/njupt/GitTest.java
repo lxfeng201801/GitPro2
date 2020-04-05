255fdf60c38060c457ad57425303db763898d162
lxｆｅｎｇ２０１８０１ <>
2020-04-05 15:56:43 +0800

    dev1_001

--- GitPro1/src/njupt/GitTest.java
+++ GitPro1/src/njupt/GitTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class GitTest {
 	public static void main(String[] args) throws IOException {
-		 System.out.println("linegit1:"); 
+		 System.out.println("linegit1-branc2:");
 		 System.out.println("linegit2:");
 		 System.out.println("linegit3:");
 		 System.out.println("linegit5idea:");
